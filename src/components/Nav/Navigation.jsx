99943b8766df225e22f18a261a937b3b7776f4b4
Lucas Gold <lucasgold01@gmail.com>
2026-09-01 01:32:05 -0400

    nav content update

--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -11,13 +11,18 @@ const listVariants = {
 const navItems = [
   { name: "home", href: "/", internal: true, color: "#FF2D95", icon: "fa-solid fa-house" },
 
-  { name: "projects", href: "/projects", internal: true, color: "#FF2DFE", icon: "fa-solid fa-diagram-project" },
+  { name: "projects", href: "/projects", internal: true, color: "#DD3AAB", icon: "fa-solid fa-diagram-project" },
 
-  { name: "experience", href: "/experience", internal: true, color: "#B026FF", icon: "fa-solid fa-briefcase" },
+  { name: "experience", href: "/experience", internal: true, color: "#BB47C0", icon: "fa-solid fa-briefcase" },
 
-  { name: "email", action: "email_modal", internal: true, color: "#4C5CFF", icon: "fa-solid fa-envelope" },
+  { name: "email", action: "email_modal", internal: true, color: "#9954D6", icon: "fa-solid fa-envelope" },
 
-  { name: "github", href: "https://github.com/lucas-gold", internal: false, color: "#2F7BFF", icon: "fa-brands fa-github", target: "_blank" },
+  { name: "github", href: "https://github.com/lucas-gold", internal: false, color: "#775FE1", icon: "fa-brands fa-github", target: "_blank" },
+
+  { name: "fennel ai", href: "https://fennel.garden", internal: false, color: "#556CF0", icon: "fa-solid fa-robot", target: "_blank" },
+
+  { name: "harvest rush", href: "https://harvestrush.fennel.garden", internal: false, color: "#2F7BFF", icon: "fa-solid fa-cow", target: "_blank" },
+
 ];
 
 
